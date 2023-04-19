02530646a74f7e47158fa08d524db30161385932
youssef salah <youssefsalahcs@gmail.com>
2023-04-19 13:39:38 +0200

    database setup

---
+++ database/migrations/2023_04_19_014134_create_courses_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('name');
+            $table->foreignId('course_rule_id')->references('id')->on('course_rules')->constrained();
+            $table->foreignId('department_id')->references('id')->on('departments')->constrained();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('courses');
+    }
+};
