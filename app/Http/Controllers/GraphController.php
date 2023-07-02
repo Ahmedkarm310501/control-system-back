6b262381ba07e08279c2f8fa2ebb31fb00f8502c
youssef salah <youssefsalahcs@gmail.com>
2023-07-02 14:49:21 +0300

    update dashboard service

--- app/Http/Controllers/GraphController.php
+++ app/Http/Controllers/GraphController.php
@@ -33,24 +33,6 @@ class GraphController extends Controller
         $graph_three = $courseGradeService->graphThree($course_semester);
         return $this->success($graph_three,200,'Graph three');
     }
-    public function graphCompareOne(CompareTwoSemesterRequest $request,DashboardService $courseGradeService)
-    {
-        $course_semester = $request->validated();
-        $graph_compare_one = $courseGradeService->graphCompareOne($course_semester);
-        return $this->success($graph_compare_one,200,'Graph compare one');
-    }
-    public function graphCompareTwo(CompareTwoSemesterRequest $request,DashboardService $courseGradeService)
-    {
-        $course_semester = $request->validated();
-        $graph_compare_two = $courseGradeService->graphCompareTwo($course_semester);
-        return $this->success($graph_compare_two,200,'Graph compare two');
-    }
-    public function graphCompareThree(CompareTwoSemesterRequest $request,DashboardService $courseGradeService)
-    {
-        $course_semester = $request->validated();
-        $graph_compare_three = $courseGradeService->graphCompareThree($course_semester);
-        return $this->success($graph_compare_three,200,'Graph compare three');
-    }
     public function raafaGrades(RafaaGradesRequest $request,DashboardService $courseGradeService)
     {
         $raafa_details = $request->validated();
