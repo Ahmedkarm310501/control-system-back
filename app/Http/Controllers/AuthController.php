1c2eddc2e1fd57aae473b05b8f882f00d71aeffa
Ahmed Karam <ahmedkarm749@gmail.com>
2023-07-06 18:19:04 +0300

    handel is active status in login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -11,15 +11,18 @@ class AuthController extends Controller
 {
     use HttpResponses;
 
-    public function login(LoginRequest $request , AuthService $authService) {
-        $data = $authService->login($request->validated());
-        if (!$data) {
-            return $this->error('invalid credentials', 401);
+    public function login(LoginRequest $request, AuthService $authService)
+    {
+        try {
+            $data = $authService->login($request->validated());
+            return $this->success($data, 200, 'login successful');
+        } catch (\Exception $e) {
+            return $this->error($e->getMessage(), $e->getCode());
         }
-        return $this->success($data, 200, 'login successful');
     }
 
-    public function logout(Request $request) {
+    public function logout(Request $request)
+    {
         $request->user()->currentAccessToken()->delete();
         return $this->successMessage('logout successful');
     }
--- app/Services/AuthService.php
+++ app/Services/AuthService.php
@@ -1,17 +1,22 @@
 <?php
 
 namespace App\Services;
+
 use App\Models\User;
+use Exception;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\ValidationException;
+
 class AuthService
 {
     public function login($userData)
     {
         $user = User::where('email', $userData['email'])->first();
-        if (! $user || ! Hash::check($userData['password'], $user->password)) {
-            return null;
-        }else{
+        if (!$user || !Hash::check($userData['password'], $user->password)) {
+            throw new \Exception('invalid credentials', 401);
+        } else if ($user->is_active == 0) {
+            throw new \Exception('user is not active', 403);
+        } else {
             $token = $user->createToken($userData['email'])->plainTextToken;
             return [
                 'user' => $user,
