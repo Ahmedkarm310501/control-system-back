30033162799a8b1e6302f17857ef2411bfbb4df6
Ahmed Karam <ahmedkarm749@gmail.com>
2023-07-09 21:28:02 +0300

    add log extra grades

--- database/migrations/2023_04_19_105527_create_course_semesters_table.php
+++ database/migrations/2023_04_19_105527_create_course_semesters_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -17,6 +16,10 @@ return new class extends Migration
             $table->foreignId('semester_id')->references('id')->on('semesters')->constrained();
             $table->string('stud_names')->nullable();
             $table->string('stud_grades')->nullable();
+            $table->string('stud_term_work')->nullable();
+            $table->string('stud_exam_work')->nullable();
+            $table->string('stud_extra_grade')->nullable();
+
             $table->unique(['course_id', 'semester_id'], 'course_semester_unique');
             $table->timestamps();
         });
